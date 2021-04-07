c8c45775fdb0439e005f5ddaf1ca4381a1acfd9b
robinmalkus <robinmalku@gmail.com>
2021-04-07 14:36:14 -0400

    convert a string to a number

--- index.js
+++ index.js
@@ -2,16 +2,27 @@
 //Define the parameters
 //What are the results?
 //Examples
+//1) multiplies two numbers when one is stringified
+//2) treats null values as zeroes and includes them in the calculation
+//3) ignores undefined and empty string values and continues operation
+//4) ignores non-numeric values  and continues operation
+//5) returns NaN if the array does not represent a proper operation
 
 // function calculate (HazyCalculator)
-const HazyCalculator = (array1) => {
+function calculate (string1, HazyCalculator){
+//convert a string to a number
+parseInt ('1', 1)
+parseInt ('2', 2)
+parseInt ('3', 3)
+parseInt ('4', 4)
+parseInt ('5', 5)
+parseInt ('6', 6)
+parseInt ('7', 7)
+parseInt ('8', 8)
+parseInt ('9', 9)
+// multiplies two numbers when one is stringified
+if (HazyCalculator) { 
 }
-
-// parameters (`+`, `-`, `*`, `/`)
-    let operand = (`+`, `-`, `*`, `/`)
-
-    
-// Stringfield numbers treated as numbers (eg. `'2'`)
 // Undefined values ignored
 // Empty string values ignored
 // Non-numeric values ingnored
