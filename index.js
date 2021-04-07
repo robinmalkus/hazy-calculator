4c9759b59b22b27cee383f6f4b84b7404a07a23d
robinmalkus <robinmalku@gmail.com>
2021-04-07 11:33:17 -0400

    Pseduo code

---
+++ index.js
@@ -0,0 +1,11 @@
+//Pseduo code
+//Define the parameters
+//What are the results?
+//Examples
+
+// function calculate (array1)
+// parameters (`+`, `-`, `*`, `/`)
+// Stringfield numbers treated as numbers (eg. `'2'`)
+// Undefined values ignored
+// Empty string values ignored
+// Non-numeric values ignored
